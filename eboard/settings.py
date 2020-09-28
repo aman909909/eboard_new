67c7533da70ab4aec33d8fb32264ddcf6d94f11e
Richa Singh <singhricha234@gmail.com>
2020-09-28 23:29:45 +0530

    Add drf

--- eboard/settings.py
+++ eboard/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'base',
 ]
 
@@ -70,7 +71,6 @@ DATABASES = {
         },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
